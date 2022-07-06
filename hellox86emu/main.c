e3385c0e74929c617776fbd83f89b874d9abdc42
xcbosa mbp16 <xcbosa@forgetive.org>
2022-07-06 22:55:10 +0800

    Initial Commit

---
+++ hellox86emu/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  hellox86emu
+//
+//  Created by 邢铖 on 2022/7/6.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
